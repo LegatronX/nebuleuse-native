8d4a069f343a0a334c8ad4b82e1bb363aeac790b
SnailX <50078487+LegatronX@users.noreply.github.com>
2026-07-25 20:22:03 +0200

    feat: app SwiftUI entry

---
+++ NebuleuseNative/NebuleuseNativeApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct NebuleuseNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+                .statusBarHidden(true)
+        }
+    }
+}
